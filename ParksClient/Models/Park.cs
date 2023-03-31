b4897e70c25d90d06ef3658346d8c5f92c653168
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-03-31 11:52:54 -0700

    add models

---
+++ ParksClient/Models/Park.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Parks.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Description { get; set; }
+    public string InterestingFacts { get; set; }
+    public string PopularSights { get; set; }
+
+    public static List<Park> GetParks()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Park> parkList = JsonConvert.DeserializeObject<List<Park>>(jsonResponse.ToString());
+
+      return parkList;
+    }
+
+    public static Park GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Park park = JsonConvert.DeserializeObject<Park>(jsonResponse.ToString());
+
+      return park;
+    }
+
+    public static void Post(Park park)
+    {
+      string jsonPark = JsonConvert.SerializeObject(park);
+      ApiHelper.Post(jsonPark);
+    }
+
+    public static void Put(Park park)
+    {
+      string jsonPark = JsonConvert.SerializeObject(park);
+      ApiHelper.Put(park.ParkId, jsonPark);
+    }
+
+    public static void Delete (int id)
+    {
+      ApiHelper.Delete(id);
+    }
+
+    public static List<Park> GetParks(int page)
+    {
+      var apiCallTask = ApiHelper.GetParks(page);
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Park> parkList = JsonConvert.DeserializeObject<List<Park>>(jsonResponse.ToString());
+
+      return parkList;
+    }
+  }
+}
